51d1e7e4cf6b2dce183bab74eeb3cdbf6b87ba65
atanasovv <atanasovv@ryanair.com>
2016-08-29 11:47:55 +0100

    added interactivePosition model

--- squirrel-master/entrypoint.sh
+++ squirrel-master/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 printf "Waiting for etcd to start\n"
+httpstatus="000"
 while [ $httpstatus -ne "200" ]; do
-  httpstatus=$(curl -s -o /dev/null -I -w "%{http_code}" http://etcd:2379/version)
+  httpstatus=$(curl -s -o /dev/null -L -w "%{http_code}" http://etcd:2379/version)
 done
 
 keyexists=$(curl -s -o /dev/null -I -w "%{http_code}" http://etcd:2379/v2/keys/squirrel/master_ifce)
@@ -12,14 +13,18 @@ if [ keyexists != 200 ]
     echo "Setting keys up"
     curl http://etcd:2379/v2/keys/squirrel/master_ifce -XPUT -d value="eth0"
     curl http://etcd:2379/v2/keys/squirrel/master/emulated_subnet -XPUT -d value="10.0.4.0/24"
-    curl http://etcd:2379/v2/keys/squirrel/master/mobility_manager -XPUT -d value="StaticUniformPositions"
-    curl http://etcd:2379/v2/keys/squirrel/master/mobility_manager_config_path -XPUT -d value="/squirrel/master/StaticUniformPositions.1"
-    curl http://etcd:2379/v2/keys/squirrel/master/StaticUniformPositions.1/spacing -XPUT -d value="200"
-    curl http://etcd:2379/v2/keys/squirrel/master/StaticUniformPositions.1/shape -XPUT -d value="Linear"
+    # curl http://etcd:2379/v2/keys/squirrel/master/mobility_manager -XPUT -d value="StaticUniformPositions"
+    curl http://etcd:2379/v2/keys/squirrel/master/mobility_manager -XPUT -d value="InteractivePositions"
+    # curl http://etcd:2379/v2/keys/squirrel/master/mobility_manager_config_path -XPUT -d value="/squirrel/master/StaticUniformPositions.1"
+    curl http://etcd:2379/v2/keys/squirrel/master/mobility_manager_config_path -XPUT -d value="/squirrel/master/InteractivePositions"
+    curl http://etcd:2379/v2/keys/squirrel/master/InteractivePositions/laddr -XPUT -d value=":9999"
+    # curl http://etcd:2379/v2/keys/squirrel/master/StaticUniformPositions.1/spacing -XPUT -d value="200"
+    # curl http://etcd:2379/v2/keys/squirrel/master/StaticUniformPositions.1/shape -XPUT -d value="Linear"
     curl http://etcd:2379/v2/keys/squirrel/master/september -XPUT -d value="September0th"
     curl http://etcd:2379/v2/keys/squirrel/master/september -XPUT -d value="September0th"
     curl http://etcd:2379/v2/keys/squirrel/master_uri -XPUT -d value="squirrel-master"
     curl http://etcd:2379/v2/keys/squirrel/worker_tap_name -XPUT -d value="tap0"
+
 fi
 
 
--- squirrel-worker/entrypoint.sh
+++ squirrel-worker/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 printf "Waiting for etcd to start\n"
+httpstatus="000"
 while [ $httpstatus -ne "200" ]; do
   httpstatus=$(curl -s -o /dev/null -I -w "%{http_code}" http://etcd:2379/v2/keys/squirrel/master_ifce)
 done
 
+# printf "Waiting for squirrel-master to start\n"
+status=1
+while [ $status -ne 0 ]; do
+  nc -vz squirrel-master 1234
+  status=$?
+done
+
 # babeld -d 3 eth0
 
-(cd /tmp && ./squirrel-worker -m squirrel-master -i 1 -t tap0)
+(cd /tmp && ./squirrel-worker -m squirrel-master -i 1 -t tap0 &)
 
 while true
 do
